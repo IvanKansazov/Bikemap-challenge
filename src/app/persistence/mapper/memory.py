3087141c17654855ee0382a410e42672bf275b96
Ivan Kansazov <ivan.kansazov@smartwitness.com>
2022-12-15 11:54:35 +0200

    Implement todo getter from database

--- src/app/persistence/mapper/memory.py
+++ src/app/persistence/mapper/memory.py
@@ -4,17 +4,18 @@ from datetime import datetime, timezone
 from entities import TodoEntry
 from persistence.mapper.errors import EntityNotFoundMapperError, CreateMapperError
 from persistence.mapper.interfaces import TodoEntryMapperInterface
+from persistence.db import DB
 
 
 class MemoryTodoEntryMapper(TodoEntryMapperInterface):
     _storage: dict
 
-    def __init__(self, storage: dict) -> None:
+    def __init__(self, storage: DB) -> None:
         self._storage = storage
 
     async def get(self, identifier: int) -> TodoEntry:
         try:
-            return self._storage[identifier]
+            return self._get_row(identifier)
         except KeyError:
             raise EntityNotFoundMapperError(f"Entity `id:{identifier}` was not found.")
 
@@ -41,3 +42,11 @@ class MemoryTodoEntryMapper(TodoEntryMapperInterface):
             identifier = randint(1, 10_000)
 
         return identifier
+
+    def _get_row(self, identifier):
+        query = "SELECT id, summary, detail, label, updated_at, created_at FROM todos WHERE id=%s LIMIT 1"
+        self._storage.cursor.execute(query, (identifier,))
+        todo_row = self._storage.cursor.fetchone()
+        if not todo_row:
+            raise KeyError
+        return todo_row
